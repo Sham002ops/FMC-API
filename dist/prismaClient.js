ab20862023860deecdd02b5b30890317c3c3697e
Sham002ops <shamband420@gmail.com>
2025-08-13 18:51:19 +0530

    Remove dist folder from repository

--- dist/prismaClient.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_1 = require("./generated/prisma"); // adjust path if needed
-const prisma = new prisma_1.PrismaClient();
-exports.default = prisma;
